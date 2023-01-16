985ec9862bbf43afdb327b17343a5bdad2669353
Jigar Shethia <jigar.shethia@biofourmis.com>
2023-01-16 16:03:16 +0530

    Added changes for SPM Depandacy.

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-numerics.git", .upToNextMajor(from: "1.0.0"))
-        
-
+    
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
