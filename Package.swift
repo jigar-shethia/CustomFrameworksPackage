524374f06098a2255ae7867497878d10352cdd77
Jigar Shethia <jigar.shethia@biofourmis.com>
2023-01-17 17:58:22 +0530

    Removed SPM module,

--- Package.swift
+++ Package.swift
@@ -14,10 +14,7 @@ let package = Package(
             name: "CustomFrameworksPackage",
             targets: ["CustomFrameworksPackage"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-numerics.git", .upToNextMajor(from: "1.0.0"))
-    
-    ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
